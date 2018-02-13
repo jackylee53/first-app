c06fae2bd1b561e248375b949bfcae56a28adb41
Henry_yuan <870729984@qq.com>
2018-02-13 17:19:37 +0800

    默认的

--- homework/test.py
+++ homework/test.py
@@ -1,12 +1,17 @@
-def func():
-    prefix = "Good Morning"
-    def func2(name):
-        print(prefix, name)
-    return func2
-f = func()
-f("henry")
-f("tom")
-print(dir(f))
-print(f.__closure__)
-print(type(f.__closure__[0]))
-print(f.__closure__[0].cell_contents)
+def flatten(nested):
+    try:
+        # 如果是字符串，那么手动抛出TypeError。
+        if isinstance(nested, str):
+            raise TypeError
+        for sublist in nested:
+            for element in flatten(sublist):
+                print('got:', element)
+    except TypeError:
+        yield nested
+
+
+L = ['aaadf', [1, 2, 3], 2, 4, [5, [6, [8, [9]], 'ddf'], 7]]
+for num in flatten(L):
+    print(num)
+
+print(isinstance(L, str))
