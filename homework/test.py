623c820b6343beb60d5cf4dccdfd47655b227d50
henryyuan <870729984@qq.com>
2018-02-21 16:52:15 +0800

    13

--- homework/test.py
+++ homework/test.py
@@ -1,6 +1,3 @@
-f = open(file='test', mode='r+', encoding='utf-8')
-data = f.read()
-f.seek(0)
-new_data = data.replace('jenry', 'henry')  # 将文件中jenry字符替换为ham
-f.write(new_data)
-f.close()
+import string, random
+s = string.ascii_lowercase + string.digits
+print(''.join(random.sample(s,5)))
