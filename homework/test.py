0649ded29fa2ab96e0e3b42757d684996416b9db
Henry_yuan <870729984@qq.com>
2018-03-09 17:40:44 +0800

    sdf

--- homework/test.py
+++ homework/test.py
@@ -1,23 +1,28 @@
-class Company:  # 父类
-    name = 'Company' # 父类的name数据属性
-    def transaction(self):  # 类的函数属性，转账
-        print("Transaction monry to your Comany")
-        self.purchase()  # 调用实例的采购函数
+class G(object):
+    def show(self):
+        print("I am G")
 
+class F(G):
+    def show(self):
+        print("I am F")
 
-    def purchase(self):  # 类的函数属性，采购
-        print("Your Comany can purchase something.")
+class E(G):
+    def show(self):
+        print("I am E")
 
+class D(G):
+    def show(self):
+        print("I am D")
 
-class Baidu(Company):  # Baidu类继承Company父类
-    name = 'Baidu'  # 派生一个自己的数据属性
-    def purchase(self):  # 派生一个自己的采购函数属性
-        print("My Comany can purchase Phone.")
+class C(F):
+    def show(self):
+        print("I am C")
 
-    def website(self):  # 派生一个自己专用的website函数属性。父中没有
-        print("www.badiu.com")
+class B(E):
+    def show(self):
+        print("I am B")
 
+class A(B, C ,D):
+    pass
 
-baidu = Baidu()  # 使用中国类创建一个百度的实例
-baidu.website()
-print(Baidu.mro())
+print(A.mro())
