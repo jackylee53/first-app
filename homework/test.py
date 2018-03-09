c2c79924cb9165ac5c97440240a0f308f107878a
Henry_yuan <870729984@qq.com>
2018-03-09 15:19:36 +0800

    sdf

--- homework/test.py
+++ homework/test.py
@@ -1,25 +1,23 @@
-class China:
-    company = 'BAIDU'
-    def __init__(self,card, balance, amount):
-        self.card = card
-        self.balance = balance
-        self.amount = amount
+class Company:  # 父类
+    name = 'Company' # 父类的name数据属性
+    def transaction(self):  # 类的函数属性，转账
+        print("Transaction monry to your Comany")
+        self.purchase()  # 调用实例的采购函数
 
 
-    def transaction(self, another):
-        another.balance -= self.amount
-
-class American:
-    company = 'IBM'
-    def __init__(self, card, balance, amount):
-        self.card = card
-        self.balance = balance
-        self.amount = amount
-
-    def transaction(self, another):
-        another.balance -= self.amount
+    def purchase(self):  # 类的函数属性，采购
+        print("Your Comany can purchase something.")
 
 
-baidu = China('123456', 23, 10)
-ibm = American('654321', 23, 10)
+class Baidu(Company):  # Baidu类继承Company父类
+    name = 'Baidu'  # 派生一个自己的数据属性
+    def purchase(self):  # 派生一个自己的采购函数属性
+        print("My Comany can purchase Phone.")
 
+    def website(self):  # 派生一个自己专用的website函数属性。父中没有
+        print("www.badiu.com")
+
+
+baidu = Baidu()  # 使用中国类创建一个百度的实例
+baidu.website()
+print(Baidu.mro())
